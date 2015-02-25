0f78faf9f47b055c73807ac30d85cb8511aca799
Rasmus Einarsson <rasmus.einarsson@sp.se>
2015-02-25 16:49:05 +0100

    Make simple input specification example

--- ModuleConfig.py
+++ ModuleConfig.py
@@ -1,6 +1,17 @@
+from IronPythonTest import List, Number, InputSpecification
+
 name = "Renobuild"
 description = "Description of the module"
 moduleId = 123
+path = '....xls'
+
+def input_specification():
+    inputs = InputSpecification()
+    inputs.add(Number(max=32))
+    l = List()
+    l.add(Number(min=0))
+    inputs.add(l)
+    return inputs
 
 def run(indata, excel):
-    return indata + excel
+    return "test"
