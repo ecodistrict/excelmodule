d98f798eea87cd55ff28187b264fe15e02c0c66c
Joma564 <johannes.markstrom@sp.se>
2015-04-08 11:54:02 +0200

    Renobuild excel module

---
+++ RenobuildModule/RenobuildModule/SourceCode/IMB_Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenobuildModule
+{
+    public class IMB_Settings
+    {
+        public string subScribedEventName;
+        public string publishedEventName;
+        public string serverAdress;
+        public int port;
+        public string federation;
+    }
+}
---
+++ RenobuildModule/RenobuildModule/SourceCode/RenobuildModule.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Yaml.Serialization;
+using Ecodistrict.Messaging;
+
+namespace RenobuildModule
+{
+    class RenobuildModule //: ExcelModule
+    {
+        protected InputSpecification GetInputSpec(string kpiId)
+        {
+            InputSpecification iSpec = new InputSpecification();
+
+            switch(kpiId)
+            {
+                case "something":
+                    //Your input spec def.
+                    break;
+                case "somethingElse":
+                    //Your input spec def.
+                    break;
+                default:
+                    throw new ApplicationException(String.Format("No input specification for kpiId '{0}' could be found.", kpiId));
+            }
+
+            return iSpec;
+        }
+
+        protected Outputs CalculateKPI(Dictionary<string,object> indata, string kpiId)  //CExcel excelDoc
+        {
+            Outputs outputs = new Outputs();
+
+            switch(kpiId)
+            {
+                case "something":
+                    //Do your calculations here.
+                    break;
+                case "somethingElse":
+                    //Do your calculations here.
+                    break;
+                default:
+                    throw new ApplicationException(String.Format("No calcualtion procedure could be found for '{0}'", kpiId));
+            }
+            
+            return outputs;
+        }
+
+        public bool Init(string IMB_config_path, string Module_config_path)
+        {
+            try
+            {
+                Init_IMB(IMB_config_path);
+                Init_Module(Module_config_path);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }            
+        }
+
+        public void Init_IMB(string IMB_config_path)
+        {
+            try
+            {
+                var serializer = new YamlSerializer();
+                var imb_settings = serializer.DeserializeFromFile(IMB_config_path, typeof(IMB_Settings))[0];
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error reading the IMB configuration file", ex);
+            }
+        }
+
+        public void Init_Module(string Module_config_path)
+        {
+            try
+            {
+                var serializer = new YamlSerializer();
+                var module_settings = serializer.DeserializeFromFile(Module_config_path, typeof(Module_Settings))[0];
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error reading the module configuration file", ex);
+            }
+        }
+    }
+}
