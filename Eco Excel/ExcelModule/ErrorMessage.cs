bf85b882f9cc5cff83e1091fe9243c7b29040030
PerBerg <per.berg@sp.se>
2015-04-08 14:40:39 +0200

    Error Event

---
+++ Eco Excel/ExcelModule/ErrorMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecodistrict.Excel
+{
+    public class ErrorMessage:EventArgs
+    {
+        public string Message;
+    }
+}
