49ebab776807b0168a8da12321f0fd265558d925
Joma564 <johannes.markstrom@sp.se>
2015-09-21 13:11:35 +0200

    Local HUB, inputspec changes, updated module desc.

--- Green_Berlin_BAF_Module/Green_Berlin_BAF_Module/SourceCode/Green_BerlinBAF_Module.cs
+++ Green_Berlin_BAF_Module/Green_Berlin_BAF_Module/SourceCode/Green_BerlinBAF_Module.cs
@@ -91,56 +91,56 @@ namespace Green_BerlinBAF_Module
 
             //Sealed Surfaces
             InputGroup sealedSurfaces = new InputGroup("Sealed surfaces", ++ipgOrder);
-            sealedSurfaces.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0, null, 0));
-            sealedSurfaces.Add(amount, new Number("Amount", 2, 0, "m\u00b2", 0));
+            //sealedSurfaces.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0, null, 0));
+            sealedSurfaces.Add(amount, new Number("Amount (weighted 0)", 2, 0, "m\u00b2", 0));
             iSpec.Add(sealed_surfaces, sealedSurfaces);
 
             //Partially Sealed Surfaces
             InputGroup partiallySealedSurfaces = new InputGroup("Partially sealed surfaces", ++ipgOrder);
-            partiallySealedSurfaces.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0.3, null, 0));
-            partiallySealedSurfaces.Add(amount, new Number("Amount", 2, 0, "m\u00b2", 0));
+            //partiallySealedSurfaces.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0.3, null, 0));
+            partiallySealedSurfaces.Add(amount, new Number("Amount (weighted 0.3)", 2, 0, "m\u00b2", 0));
             iSpec.Add(partially_sealed_surfaces, partiallySealedSurfaces);
 
             //Semi-open Surfaces
             InputGroup semiOpenSurfaces = new InputGroup("Semi-open surfaces", ++ipgOrder);
-            semiOpenSurfaces.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0.5, null, 0));
-            semiOpenSurfaces.Add(amount, new Number("Amount", 2, 0, "m\u00b2", 0));
+            //semiOpenSurfaces.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0.5, null, 0));
+            semiOpenSurfaces.Add(amount, new Number("Amount (weighted 0.5)", 2, 0, "m\u00b2", 0));
             iSpec.Add(semiOpenSurfacesStr, semiOpenSurfaces);
 
             //Surfaces with vegetation unconnected to the soil below and with < 80 mm of soil covering
             InputGroup swvutsBelow80 = new InputGroup("Surfaces with vegetation unconnected to the soil below and with < 80 mm of soil covering", ++ipgOrder);
-            swvutsBelow80.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0.5, null, 0));
-            swvutsBelow80.Add(amount, new Number("Amount", 2, 0, "m\u00b2", 0));
+            //swvutsBelow80.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0.5, null, 0));
+            swvutsBelow80.Add(amount, new Number("Amount (weighted 0.5)", 2, 0, "m\u00b2", 0));
             iSpec.Add(swvutsBelow80Str, swvutsBelow80);
 
             //Surfaces with vegetation unconnected to the soil below and with > 80 mm of soil covering
             InputGroup swvutsAbove80 = new InputGroup("Surfaces with vegetation unconnected to the soil below and with > 80 mm of soil covering", ++ipgOrder);
-            swvutsAbove80.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0.7, null, 0));
-            swvutsAbove80.Add(amount, new Number("Amount", 2, 0, "m\u00b2", 0));
+            //swvutsAbove80.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0.7, null, 0));
+            swvutsAbove80.Add(amount, new Number("Amount (weighted 0.7)", 2, 0, "m\u00b2", 0));
             iSpec.Add(swvutsAbove80Str, swvutsAbove80);
 
             //Surfaces with vegetation connected to the soil below
             InputGroup swvConnectedToSoilBelow = new InputGroup("Surfaces with vegetation connected to the soil below", ++ipgOrder);
-            swvConnectedToSoilBelow.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 1.0, null, 0));
-            swvConnectedToSoilBelow.Add(amount, new Number("Amount", 2, 0, "m\u00b2", 0));
+            //swvConnectedToSoilBelow.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 1.0, null, 0));
+            swvConnectedToSoilBelow.Add(amount, new Number("Amount (weighted 1.0)", 2, 0, "m\u00b2", 0));
             iSpec.Add(swvConnectedToSoilBelowStr, swvConnectedToSoilBelow);
 
             //Rainwater infiltration per m² of runoff area
             InputGroup rainwaterInfiltrationpSqrmRunoffArea = new InputGroup("Rainwater infiltration per m² of runoff area", ++ipgOrder);
-            rainwaterInfiltrationpSqrmRunoffArea.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0.2, null, 0));
-            rainwaterInfiltrationpSqrmRunoffArea.Add(amount, new Number("Amount", 2, 0, "m\u00b2", 0));
+            //rainwaterInfiltrationpSqrmRunoffArea.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0.2, null, 0));
+            rainwaterInfiltrationpSqrmRunoffArea.Add(amount, new Number("Amount (weighted 0.2)", 2, 0, "m\u00b2", 0));
             iSpec.Add(rainwaterInfiltrationpSqrmRunoffAreaStr, rainwaterInfiltrationpSqrmRunoffArea);
 
             //Vertical greenery up to a maximum of 10 m in height
             InputGroup vgtm10mHeight = new InputGroup("Vertical greenery up to a maximum of 10 m in height", ++ipgOrder);
-            vgtm10mHeight.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0.5, null, 0));
-            vgtm10mHeight.Add(amount, new Number("Amount", 2, 0, "m\u00b2", 0));
+            //vgtm10mHeight.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0.5, null, 0));
+            vgtm10mHeight.Add(amount, new Number("Amount (weighted 0.5)", 2, 0, "m\u00b2", 0));
             iSpec.Add(vgtm10mHeightStr, vgtm10mHeight);
 
             //Greenery on rooftop
             InputGroup greeneryOnRooftop = new InputGroup("Greenery on rooftop", ++ipgOrder);
-            greeneryOnRooftop.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0.7, null, 0));
-            greeneryOnRooftop.Add(amount, new Number("Amount", 2, 0, "m\u00b2", 0));
+            //greeneryOnRooftop.Add(weighting_factor_per_m2, new Number("Weighting factor per m²", 1, 0.7, null, 0));
+            greeneryOnRooftop.Add(amount, new Number("Amount (weighted 0.7)", 2, 0, "m\u00b2", 0));
             iSpec.Add(greeneryOnRooftopStr, greeneryOnRooftop);
             
             
@@ -171,12 +171,13 @@ namespace Green_BerlinBAF_Module
             }
             #endregion
 
+            #region Area types data
             #region Sealed Surfaces
             if (indata[sealed_surfaces] is InputGroup)
             {
                 Dictionary<string, Input> ipg = (indata[sealed_surfaces] as InputGroup).GetInputs();
-                if (ipg[weighting_factor_per_m2] is Number)
-                    Set("Blad1", "B12", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
+                //if (ipg[weighting_factor_per_m2] is Number)
+                //    Set("Blad1", "B12", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
 
                 if (ipg[amount] is Number)
                     Set("Blad1", "C12", (ipg[amount] as Number).GetValue(), ref exls);
@@ -187,8 +188,8 @@ namespace Green_BerlinBAF_Module
             if (indata[partially_sealed_surfaces] is InputGroup)
             {
                 Dictionary<string, Input> ipg = (indata[partially_sealed_surfaces] as InputGroup).GetInputs();
-                if (ipg[weighting_factor_per_m2] is Number)
-                    Set("Blad1", "B13", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
+                //if (ipg[weighting_factor_per_m2] is Number)
+                //    Set("Blad1", "B13", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
 
                 if (ipg[amount] is Number)
                     Set("Blad1", "C13", (ipg[amount] as Number).GetValue(), ref exls);
@@ -199,8 +200,8 @@ namespace Green_BerlinBAF_Module
             if (indata[semiOpenSurfacesStr] is InputGroup)
             {
                 Dictionary<string, Input> ipg = (indata[semiOpenSurfacesStr] as InputGroup).GetInputs();
-                if (ipg[weighting_factor_per_m2] is Number)
-                    Set("Blad1", "B14", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
+                //if (ipg[weighting_factor_per_m2] is Number)
+                //    Set("Blad1", "B14", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
 
                 if (ipg[amount] is Number)
                     Set("Blad1", "C14", (ipg[amount] as Number).GetValue(), ref exls);
@@ -211,8 +212,8 @@ namespace Green_BerlinBAF_Module
             if (indata[swvutsBelow80Str] is InputGroup)
             {
                 Dictionary<string, Input> ipg = (indata[swvutsBelow80Str] as InputGroup).GetInputs();
-                if (ipg[weighting_factor_per_m2] is Number)
-                    Set("Blad1", "B15", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
+                //if (ipg[weighting_factor_per_m2] is Number)
+                //    Set("Blad1", "B15", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
 
                 if (ipg[amount] is Number)
                     Set("Blad1", "C15", (ipg[amount] as Number).GetValue(), ref exls);
@@ -223,8 +224,8 @@ namespace Green_BerlinBAF_Module
             if (indata[swvutsAbove80Str] is InputGroup)
             {
                 Dictionary<string, Input> ipg = (indata[swvutsAbove80Str] as InputGroup).GetInputs();
-                if (ipg[weighting_factor_per_m2] is Number)
-                    Set("Blad1", "B19", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
+                //if (ipg[weighting_factor_per_m2] is Number)
+                //    Set("Blad1", "B19", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
 
                 if (ipg[amount] is Number)
                     Set("Blad1", "C19", (ipg[amount] as Number).GetValue(), ref exls);
@@ -235,8 +236,8 @@ namespace Green_BerlinBAF_Module
             if (indata[swvConnectedToSoilBelowStr] is InputGroup)
             {
                 Dictionary<string, Input> ipg = (indata[swvConnectedToSoilBelowStr] as InputGroup).GetInputs();
-                if (ipg[weighting_factor_per_m2] is Number)
-                    Set("Blad1", "B23", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
+                //if (ipg[weighting_factor_per_m2] is Number)
+                //    Set("Blad1", "B23", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
 
                 if (ipg[amount] is Number)
                     Set("Blad1", "C23", (ipg[amount] as Number).GetValue(), ref exls);
@@ -247,8 +248,8 @@ namespace Green_BerlinBAF_Module
             if (indata[rainwaterInfiltrationpSqrmRunoffAreaStr] is InputGroup)
             {
                 Dictionary<string, Input> ipg = (indata[rainwaterInfiltrationpSqrmRunoffAreaStr] as InputGroup).GetInputs();
-                if (ipg[weighting_factor_per_m2] is Number)
-                    Set("Blad1", "B25", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
+                //if (ipg[weighting_factor_per_m2] is Number)
+                //    Set("Blad1", "B25", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
 
                 if (ipg[amount] is Number)
                     Set("Blad1", "C25", (ipg[amount] as Number).GetValue(), ref exls);
@@ -259,8 +260,8 @@ namespace Green_BerlinBAF_Module
             if (indata[vgtm10mHeightStr] is InputGroup)
             {
                 Dictionary<string, Input> ipg = (indata[vgtm10mHeightStr] as InputGroup).GetInputs();
-                if (ipg[weighting_factor_per_m2] is Number)
-                    Set("Blad1", "B27", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
+                //if (ipg[weighting_factor_per_m2] is Number)
+                //    Set("Blad1", "B27", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
 
                 if (ipg[amount] is Number)
                     Set("Blad1", "C27", (ipg[amount] as Number).GetValue(), ref exls);
@@ -271,20 +272,22 @@ namespace Green_BerlinBAF_Module
             if (indata[greeneryOnRooftopStr] is InputGroup)
             {
                 Dictionary<string, Input> ipg = (indata[greeneryOnRooftopStr] as InputGroup).GetInputs();
-                if (ipg[weighting_factor_per_m2] is Number)
-                    Set("Blad1", "B29", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
+                //if (ipg[weighting_factor_per_m2] is Number)
+                //    Set("Blad1", "B29", (ipg[weighting_factor_per_m2] as Number).GetValue(), ref exls);
 
                 if (ipg[amount] is Number)
                     Set("Blad1", "C29", (ipg[amount] as Number).GetValue(), ref exls);
             }
+            #endregion
             #endregion
 
             double kpi = Convert.ToDouble(exls.GetCellValue("Blad1", "D35"));
 
             switch (kpiId)
             {
+                case kpi_baf:
                 case kpi_berlin_baf:
-                    outputs.Add(new Ecodistrict.Messaging.Output.Kpi(Math.Round(kpi, 2), kpi_berlin_baf, ""));
+                    outputs.Add(new Ecodistrict.Messaging.Output.Kpi(Math.Round(kpi, 2), kpiId, ""));
                     break;
                 default:
                     throw new ApplicationException(String.Format("No calculation procedure could be found for '{0}'", kpiId));
